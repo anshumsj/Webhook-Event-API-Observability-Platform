8bcb1f4403b0223e1c6b5d3583c6f85a19c5f3c0
Anshum Awasthi <anshum.230101031@iiitbh.ac.in>
2026-08-17 01:19:38 +0530

    chore: setup frontend architecture

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Webhook Event Observability Platform</h1>
+      <p>Frontend is up and running.</p>
+    </div>
+  );
+};
+
+export default Home;
